673512ddbc0e9abab5238942fdf71f7c5eb39689
Hugo <Hugoabouda@gmail.com>
2024-10-22 10:55:30 +0200

    it

--- philo.h
+++ philo.h
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
+/*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 10:32:18 by habouda           #+#    #+#             */
-/*   Updated: 2024/10/22 10:55:18 by habouda          ###   ########.fr       */
+/*   Created: 2024/10/22 10:49:43 by habouda           #+#    #+#             */
+/*   Updated: 2024/10/22 10:53:35 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
-
-void *routine()
-{
-	printf("test from threads\n");
-	sleep(3);
-	printf("yipeee\n");
-	return (NULL);
-}
-
-int main()
-{
-	pthread_t t1;
-	pthread_t t2;
-
-	pthread_create(&t1, NULL, &routine, NULL);
-	pthread_create(&t2, NULL, &routine, NULL);
-	pthread_join(t1, NULL);
-	pthread_join(t1, NULL);
-	return (0);
-}
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
