d0dd2e73a0f7fae0000675b5f039d1f673b54f10
Hugo <hugoabouda@gmail.com>
2024-11-08 22:23:15 +0100

    WORKS BUT DEATH IS NOT GOOD

--- philo_life.c
+++ philo_life.c
@@ -6,7 +6,7 @@
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 17:19:37 by habouda           #+#    #+#             */
-/*   Updated: 2024/11/08 22:15:55 by habouda          ###   ########.fr       */
+/*   Updated: 2024/11/08 22:23:09 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,7 @@ int	eat(t_data *data, t_philo *philo)
 	philo->eating = 0; 
 	return (EXIT_SUCCESS);
 }
+
 
 int	sleepge(t_data *data, t_philo *philo)
 {
